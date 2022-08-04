27edc6b01c69375bfe367eee1a23b32d3fa04056
AliAlHashemi14 <alialhashemi14@gmail.com>
2022-08-04 09:38:15 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Menu';
+  Menu: MenuItem[] = [
+    {
+      name: "Cheese Pizza",
+      category: "Pizza",
+      price: 10
+    },
+    {
+      name: "BBQ Wings",
+      category: "Wings",
+      price: 8
+    },
+    {
+      name: "Meat Pizza",
+      category: "Pizza",
+      price: 12
+    },
+    {
+      name: "Lemon Pepper Wings",
+      category: "Wings",
+      price: 8
+    },
+    {
+      name: "Breadsticks",
+      category: "Sides",
+      price: 6
+    },
+    {
+      name: "Garlic Knots",
+      category: "Sides",
+      price: 7
+    }
+  ];
+
+  getByCategory(cat:string):MenuItem[]{
+    let Result: MenuItem[] = [];
+
+    this.Menu.forEach((i:MenuItem) => {
+      if(i.category == cat){
+        Result.push(i);
+      } 
+    })
+
+
+    return Result;
+  }
 }
